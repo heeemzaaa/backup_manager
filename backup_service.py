2ee64dd59f9f6ed3c2bf5c9b9ab2ad0d6dd7070f
heeemzaaa <hamzaelkhawlani00@gmail.com>
2026-04-10 00:44:12 +0100

    performing the backup done successfully

--- backup_service.py
+++ backup_service.py
@@ -1,5 +1,6 @@
 import os
 import datetime
+import tarfile
 
 def get_timestamp():
     return datetime.now().strftime("%d/%m/%Y %H:%M")
@@ -12,6 +13,18 @@ def log(message):
         
         
 def perform_backup(path, backup_name):
+    try:
+        os.makedirs("backups", exist_ok=True)
+
+        tar_path = f"./backups/{backup_name}.tar"
+
+        with tarfile.open(tar_path, "w") as tar:
+            tar.add(path, arcname=os.path.basename(path))
+
+        log(f"Backup done for {path} in backups/{backup_name}.tar")
+
+    except Exception:
+        log(f"Error: failed backup for {path}")
 
 
 def run_service():
