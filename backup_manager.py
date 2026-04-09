8087d8c0187ed77734f8e1d52f69209edb509950
heeemzaaa <hamzaelkhawlani00@gmail.com>
2026-04-09 16:54:56 +0100

    handling the backup manager structure

---
+++ backup_manager.py
@@ -0,0 +1,17 @@
+def create(schedule: dict):
+    print("In here we will create a new backup schedule")
+
+def list_schedules():
+    print("We will list the schedules from the txt file")
+
+def delete_schedule(index):
+    print("In here we will delete a backup schedule by index")
+
+def start(schedule_id):
+    print("In here we will start the code in the background")
+
+def stop(schedule_id):
+    print("In here we will kill the process of our python code")
+
+def list_backups(path):
+    print("In here we will list the backup files in ./backups")
